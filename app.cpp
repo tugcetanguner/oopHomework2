d26646d0db284f092053d724300e9c96d737ea36
murat emir cabaroğlu <muratemir.19988@gmail.com>
2018-04-10 13:49:03 +0300

    **

--- app.cpp
+++ app.cpp
@@ -8,11 +8,9 @@ class Point{
         int y;
     public:
         Point(int _x=0,int _y=0):x(_x),y(_y){
-
         }
         void print(){
-
-            
+            cout<<"x: "<<x<<"y: "<<y<<endl;
         }
 };
 
@@ -22,15 +20,24 @@ class Circle: public Point{
         int y;
         int r;
     public:
-        Circle(){
-
+        Circle(int _x=0,int _y=0, int _r=0):Point(_x,_y),r(_r){
+            x=_x;
+            y=_y;
         }
+        void print(){
+            cout<<"x: "<<x<<"y: "<<y<<"r: "<<r<<endl;
+        }
+        
 };
 
 int main(){
 
     Point p1,p2(2,3);
-
+    Circle c1,c2(1,1,1);
     p1.print();
     p2.print();
+    c1.print();
+    c2.print();
+
+
 }
